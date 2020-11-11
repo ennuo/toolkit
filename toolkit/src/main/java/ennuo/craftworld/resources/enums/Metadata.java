b99f447f639a29177f3648c1543b4017c062ace0
ennuo <ajr.uiz2048@gmail.com>
2020-11-11 14:49:23 -0500

    Add joint file to list of valid RTypes.

--- toolkit/src/main/java/ennuo/craftworld/resources/enums/Metadata.java
+++ toolkit/src/main/java/ennuo/craftworld/resources/enums/Metadata.java
@@ -16,8 +16,8 @@ public class Metadata {
   public static Map<String, CompressionType> magic;
   static {
       magic = new HashMap<String, CompressionType>();
-              magic.put("TEX ", CompressionType.LEGACY_TEXTURE); magic.put("GTF ", CompressionType.GTF_TEXTURE); magic.put("GTFs", CompressionType.GXT_SIMPLE_TEXTURE); magic.put("GTFS", CompressionType.GXT_EXTENDED_TEXTURE); 
-        magic.put("PLNb", CompressionType.CUSTOM_COMPRESSION); magic.put("BEVb", CompressionType.CUSTOM_COMPRESSION); magic.put("ANMb", CompressionType.CUSTOM_COMPRESSION); magic.put("LVLb", CompressionType.CUSTOM_COMPRESSION); 
+        magic.put("TEX ", CompressionType.LEGACY_TEXTURE); magic.put("GTF ", CompressionType.GTF_TEXTURE); magic.put("GTFs", CompressionType.GXT_SIMPLE_TEXTURE); magic.put("GTFS", CompressionType.GXT_EXTENDED_TEXTURE); 
+        magic.put("JNTb", CompressionType.CUSTOM_COMPRESSION); magic.put("PLNb", CompressionType.CUSTOM_COMPRESSION); magic.put("BEVb", CompressionType.CUSTOM_COMPRESSION); magic.put("ANMb", CompressionType.CUSTOM_COMPRESSION); magic.put("LVLb", CompressionType.CUSTOM_COMPRESSION); 
         magic.put("CLDb", CompressionType.CUSTOM_COMPRESSION); magic.put("FSHb", CompressionType.CUSTOM_COMPRESSION); magic.put("GMTb", CompressionType.CUSTOM_COMPRESSION); magic.put("MATb", CompressionType.CUSTOM_COMPRESSION); 
         magic.put("OATb", CompressionType.CUSTOM_COMPRESSION); magic.put("SLTb", CompressionType.CUSTOM_COMPRESSION); magic.put("PALb", CompressionType.CUSTOM_COMPRESSION); magic.put("PCKb", CompressionType.CUSTOM_COMPRESSION); 
         magic.put("BPRb", CompressionType.CUSTOM_COMPRESSION); magic.put("INSb", CompressionType.CUSTOM_COMPRESSION); magic.put("MSHb", CompressionType.CUSTOM_COMPRESSION); magic.put("VOPb", CompressionType.CUSTOM_COMPRESSION);
