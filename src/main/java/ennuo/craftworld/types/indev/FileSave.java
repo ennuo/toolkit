053632b8f6e40b598bd75e4014198970a3e5896a
Aidan <ajr.uiz2048@gmail.com>
2021-10-08 22:35:36 -0400

    Precompile regex patterns

--- src/main/java/ennuo/craftworld/types/indev/FileSave.java
+++ src/main/java/ennuo/craftworld/types/indev/FileSave.java
@@ -2,11 +2,14 @@ package ennuo.craftworld.types.indev;
 
 import ennuo.craftworld.swing.FileData;
 import java.io.File;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class FileSave extends FileData {
     public int revision;
     public File directory;
     public boolean isVita = false;
+    public HashMap<String, byte[]> resources;
     
     public FileSave(File folder) {
         this.name = "Savedata";
@@ -21,25 +24,29 @@ public class FileSave extends FileData {
     }
     
     private void getLocalSave() {
-        File[] localSaves = this.directory.listFiles((dir, name) -> name.matches("littlefart\\d+"));
+        Pattern regex = Pattern.compile("littlefart\\d+");
+        File[] localSaves = this.directory.listFiles((dir, name) -> regex.matcher(name).matches());
         for (File file : localSaves)
             System.out.println(String.format("Found local save file: %s", file.getName()));
     }
     
     private void getBigSave() {
-        File[] bigSaves = this.directory.listFiles((dir, name) -> name.matches("bigfart\\d+"));
+        Pattern regex = Pattern.compile("bigfart\\d+");
+        File[] bigSaves = this.directory.listFiles((dir, name) -> regex.matcher(name).matches());
         for (File file : bigSaves)
             System.out.println(String.format("Found big save file: %s", file.getName()));
     }
     
     private void getMoonSlots() {
-        File[] moonSaves = this.directory.listFiles((dir, name) -> name.matches("moon\\d+_\\d+"));
+        Pattern regex = Pattern.compile("moon\\d+_\\d+");
+        File[] moonSaves = this.directory.listFiles((dir, name) -> regex.matcher(name).matches());
         for (File file : moonSaves)
             System.out.println(String.format("Found moon save file: %s", file.getName()));
     }
     
     private void getDownloadSlots() {
-        File[] downloadSaves = this.directory.listFiles((dir, name) -> name.matches("slot\\d+_\\d+"));
+        Pattern regex = Pattern.compile("slot\\d+_\\d+");
+        File[] downloadSaves = this.directory.listFiles((dir, name) -> regex.matcher(name).matches());
         for (File file : downloadSaves)
             System.out.println(String.format("Found download save file: %s", file.getName()));
     }
