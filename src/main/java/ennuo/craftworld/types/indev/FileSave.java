1e083fa2b60ee514e2b6428c975cfa9adb7d0b51
Aidan <ajr.uiz2048@gmail.com>
2021-10-09 16:43:39 -0400

    Append local archives to savedata manager

--- src/main/java/ennuo/craftworld/types/indev/FileSave.java
+++ src/main/java/ennuo/craftworld/types/indev/FileSave.java
@@ -1,6 +1,9 @@
 package ennuo.craftworld.types.indev;
 
+import ennuo.craftworld.memory.Bytes;
 import ennuo.craftworld.swing.FileData;
+import ennuo.craftworld.types.FileArchive;
+import ennuo.craftworld.types.FileEntry;
 import java.io.File;
 import java.util.HashMap;
 import java.util.regex.Pattern;
@@ -13,7 +16,7 @@ public class FileSave extends FileData {
     public BigProfile bigProfile;
     public LocalProfile localProfile;
     
-    public HashMap<String, byte[]> resources;
+    public HashMap<String, byte[]> resources = new HashMap<String, byte[]>();
     
     public FileSave(File folder) {
         this.name = "Savedata";
@@ -29,31 +32,52 @@ public class FileSave extends FileData {
         }
     }
     
+    private String appendArchive(File file) {
+        FileArchive archive = new FileArchive(file);
+        if (archive.isParsed) {
+            for (FileEntry entry : archive.entries)
+                this.resources.put(Bytes.toHex(entry.hash), entry.data);
+            return archive.getFatDataSource();
+        }
+        return null;
+    }
+    
     private void getLocalSave() {
         Pattern regex = Pattern.compile("littlefart\\d+");
         File[] localSaves = this.directory.listFiles((dir, name) -> regex.matcher(name).matches());
-        for (File file : localSaves)
-            System.out.println(String.format("Found local save file: %s", file.getName()));
+        if (localSaves.length == 0) {
+            System.out.println("Couldn't find any local profiles in this directory!");
+            return;
+        } else if (localSaves.length != 1) 
+            System.out.println(String.format("Multiple local profiles found, defaulting to %s", localSaves[0].getName()));
+        else System.out.println(String.format("Found local save file: %s", localSaves[0].getName())); 
+        String localProfileHash = this.appendArchive(localSaves[0]);
     }
     
     private void getBigSave() {
         Pattern regex = Pattern.compile("bigfart\\d+");
         File[] bigSaves = this.directory.listFiles((dir, name) -> regex.matcher(name).matches());
-        for (File file : bigSaves)
-            System.out.println(String.format("Found big save file: %s", file.getName()));
+        for (File file : bigSaves) {
+            FileArchive archive = new FileArchive(file);
+            System.out.println(String.format("Found big save file: %s", file.getName()));   
+        }
     }
     
     private void getMoonSlots() {
         Pattern regex = Pattern.compile("moon\\d+_\\d+");
         File[] moonSaves = this.directory.listFiles((dir, name) -> regex.matcher(name).matches());
-        for (File file : moonSaves)
-            System.out.println(String.format("Found moon save file: %s", file.getName()));
+        for (File file : moonSaves) {
+            FileArchive archive = new FileArchive(file);
+            System.out.println(String.format("Found moon save file: %s", file.getName()));   
+        }
     }
     
     private void getDownloadSlots() {
         Pattern regex = Pattern.compile("slot\\d+_\\d+");
         File[] downloadSaves = this.directory.listFiles((dir, name) -> regex.matcher(name).matches());
-        for (File file : downloadSaves)
-            System.out.println(String.format("Found download save file: %s", file.getName()));
+        for (File file : downloadSaves) {
+            FileArchive archive = new FileArchive(file);
+            System.out.println(String.format("Found download save file: %s", file.getName()));   
+        }
     }
 }
