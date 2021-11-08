f8d2babcff6cb760b65fe46c76b4f095b391d190
Aidan <ajr.uiz2048@gmail.com>
2021-11-08 08:19:53 -0500

    Add PScriptName

--- src/main/java/ennuo/craftworld/things/parts/PScriptName.java
+++ src/main/java/ennuo/craftworld/things/parts/PScriptName.java
@@ -4,9 +4,12 @@ import ennuo.craftworld.serializer.Serializable;
 import ennuo.craftworld.serializer.Serializer;
 
 public class PScriptName implements Serializable {
-
+    public String name;
+    
     public PScriptName serialize(Serializer serializer, Serializable structure) {
         PScriptName scriptName = (structure == null) ? new PScriptName() : (PScriptName) structure;
+        
+        scriptName.name = serializer.str8(scriptName.name);
         
         return scriptName;
     }
