f36fd4e869e83330bb041b303be9670bbe8940e2
Aidan <ajr.uiz2048@gmail.com>
2021-11-08 08:04:17 -0500

    Add PRef

--- src/main/java/ennuo/craftworld/things/parts/PRef.java
+++ src/main/java/ennuo/craftworld/things/parts/PRef.java
@@ -1,12 +1,26 @@
 package ennuo.craftworld.things.parts;
 
+import ennuo.craftworld.resources.enums.RType;
 import ennuo.craftworld.serializer.Serializable;
 import ennuo.craftworld.serializer.Serializer;
+import ennuo.craftworld.types.data.ResourcePtr;
 
 public class PRef implements Serializable {
-
+    ResourcePtr plan;
+    int oldLifetime;
+    int oldAliveFrames;
+    boolean childrenSelectable;
+    boolean stripChildren;
+    
+    
     public PRef serialize(Serializer serializer, Serializable structure) {
         PRef ref = (structure == null) ? new PRef() : (PRef) structure;
+        
+        ref.plan = serializer.resource(ref.plan, RType.PLAN, true);
+        ref.oldLifetime = serializer.i32(ref.oldLifetime);
+        ref.oldAliveFrames = serializer.i32(ref.oldAliveFrames);
+        ref.childrenSelectable = serializer.bool(ref.childrenSelectable);
+        ref.stripChildren = serializer.bool(ref.stripChildren);
         
         return ref;
     }
