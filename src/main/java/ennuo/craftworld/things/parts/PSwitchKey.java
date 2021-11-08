70d15fb3d7a27cd4ff0a6db7895f71c639d4231c
Aidan <ajr.uiz2048@gmail.com>
2021-11-08 08:20:55 -0500

    Add PSwitchKey

--- src/main/java/ennuo/craftworld/things/parts/PSwitchKey.java
+++ src/main/java/ennuo/craftworld/things/parts/PSwitchKey.java
@@ -4,9 +4,12 @@ import ennuo.craftworld.serializer.Serializable;
 import ennuo.craftworld.serializer.Serializer;
 
 public class PSwitchKey implements Serializable {
-
+    public int colorIndex;
+    
     public PSwitchKey serialize(Serializer serializer, Serializable structure) {
         PSwitchKey switchKey = (structure == null) ? new PSwitchKey() : (PSwitchKey) structure;
+        
+        switchKey.colorIndex = serializer.i32(switchKey.colorIndex);
         
         return switchKey;
     }
