0665853fc3179c6e7e7521a1ced863e86b61b4e8
ennuo <ajr.uiz2048@gmail.com>
2021-11-13 00:57:57 -0500

    Revision helper functions

--- src/main/java/ennuo/craftworld/resources/structs/Revision.java
+++ src/main/java/ennuo/craftworld/resources/structs/Revision.java
@@ -19,5 +19,30 @@ public class Revision {
         this.branchRevision = (short) branchRevision;
     }
     
-    public boolean isVita() { return this.branchID == 0x4431; }
+    public boolean isLBP1() { return this.head <= 0x272; }
+    public boolean isLBP1Branched() { return this.branchID == 0x4c44 && this.isLBP1(); }
+    public boolean isLBP2() { 
+        if (this.isLBP1() || this.isLBP3() || this.isVita()) return false;
+        return true;
+    }
+    public boolean isVita() { return this.branchID == 0x4431 && this.head == 0x3e2; }
+    public boolean isLBP3() { return this.head >> 0x10 != 0; }
+    
+    public boolean isAfterLBP3Revision(int revision) {
+        if (!this.isLBP3()) return false;
+        if ((this.head >> 0x10) > revision) return true;
+        return false;
+    }
+    
+    public boolean isAfterVitaBranchedRevision(int branchRevision) {
+        if (!this.isVita()) return false;
+        if (this.branchRevision > branchRevision) return true;
+        return false;
+    }
+    
+    public boolean isAfterLBP1BranchedRevision(int branchRevision) {
+        if (this.branchID != 0x4c44) return false;
+        if (this.branchRevision > branchRevision) return true;
+        return false;
+    }
 }
