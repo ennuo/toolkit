cf18b2170c49970d564fa338fdb38aa07c3717be
ennuo <ajr.uiz2048@gmail.com>
2022-03-26 20:05:01 -0400

    Give up on Darcula!
    This should fix all issues with launching on anything not JDK 11.

--- src/main/java/ennuo/toolkit/Launcher.java
+++ src/main/java/ennuo/toolkit/Launcher.java
@@ -1,24 +1,14 @@
 package ennuo.toolkit;
 
-import com.bulenkov.darcula.DarculaLaf;
 import com.github.weisj.darklaf.LafManager;
 import com.github.weisj.darklaf.theme.DarculaTheme;
 import ennuo.toolkit.configurations.Config;
 import ennuo.toolkit.windows.Toolkit;
 import java.awt.EventQueue;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class Launcher {
     public static void main(String args[]) {
-        final String OS = System.getProperty("os.name").toLowerCase();
-        if (OS.contains("win") || OS.contains("mac")) {
-            // NOTE(Aidan): Darcula crashes on Linux.
-            try { UIManager.setLookAndFeel(new DarculaLaf()); } 
-            catch (UnsupportedLookAndFeelException ex) { 
-                System.out.println("There was an error setting the Darcula theme."); 
-            }
-        } else LafManager.install(new DarculaTheme()); // NOTE(Aidan): I don't really like how this looks, but I don't like light themes!
+        LafManager.install(new DarculaTheme());
         Config.initialize();
         EventQueue.invokeLater(() -> new Toolkit().run(args).setVisible(true));
     }
