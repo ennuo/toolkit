353e5eb9ab425db8bd8f606b5dcd4af3e957ad4a
ennuo <ajr.uiz2048@gmail.com>
2022-03-27 12:18:16 -0400

    Validate strings passed into ResourceDescriptor

--- src/main/java/ennuo/craftworld/types/data/ResourceDescriptor.java
+++ src/main/java/ennuo/craftworld/types/data/ResourceDescriptor.java
@@ -3,6 +3,7 @@ package ennuo.craftworld.types.data;
 import ennuo.craftworld.resources.enums.ResourceType;
 import ennuo.craftworld.resources.structs.SHA1;
 import ennuo.craftworld.utilities.Bytes;
+import ennuo.craftworld.utilities.StringUtils;
 
 public class ResourceDescriptor {
     public ResourceType type = ResourceType.FILE_OF_BYTES;
@@ -12,40 +13,31 @@ public class ResourceDescriptor {
     
     public ResourceDescriptor() {}
     public ResourceDescriptor(ResourceType type, String str) {
-        long number = -1;
-        byte[] bytes = null;
-
-        if (str.startsWith("0x"))
-            number = Long.parseLong(str.substring(2), 16);
-        else if (str.startsWith("g"))
-            number = Long.parseLong(str.substring(1));
-        else if (str.startsWith("h"))
-            bytes = Bytes.toBytes(str.substring(1));
-        else bytes = Bytes.toBytes(str);
+        if (StringUtils.isGUID(str))
+            this.GUID = StringUtils.getLong(str);
+        else if (StringUtils.isSHA1(str))
+            this.hash = StringUtils.getSHA1(str);
+        else throw new IllegalArgumentException("Invalid resource reference passed into resource descriptor!");
 
         this.type = type;
-
-        if (bytes != null) hash = new SHA1(bytes);
-        else if (number != -1) GUID = number;
-
     }
+    
     public ResourceDescriptor(long GUID, ResourceType type) {
         this.GUID = GUID;
         this.type = type;
     }
+    
     public ResourceDescriptor(SHA1 hash, ResourceType type) {
         this.hash = hash;
         this.type = type;
     }
 
-    @Override
-    public boolean equals(Object o) {
+    @Override public boolean equals(Object o) {
         if (o == null) return false;
         return ((ResourceDescriptor) o).toString().equals(toString());
     }
 
-    @Override
-    public String toString() {
+    @Override public String toString() {
         if (hash != null) return "h" + this.hash.toString();
         else if (GUID != -1) return "g" + GUID;
         return "null";
