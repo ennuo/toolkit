d19831a0ec764e726064cf2aaa05d272f10e0bb4
ennuo <ajr.uiz2048@gmail.com>
2022-03-28 13:56:00 -0400

    Remove unused tweak menus

--- src/main/java/ennuo/toolkit/windows/editors/TweakContext.java
+++
@@ -1,5 +0,0 @@
-package ennuo.toolkit.windows.editors;
-public interface TweakContext {
-    public void context();
-    public void draw(TweakMenu tweak);
-}
--- src/main/java/ennuo/toolkit/windows/editors/TweakModel.java
+++
@@ -1,7 +0,0 @@
-package ennuo.toolkit.windows.editors;
-
-import javax.swing.tree.DefaultTreeModel;
-
-public class TweakModel extends DefaultTreeModel {
-  public TweakModel(TweakNode root) { super(root, false); }
-}
