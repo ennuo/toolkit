ee9d65b8c2c80354281f4951fe1e89ffb7e938e5
ennuo <ajr.uiz2048@gmail.com>
2022-03-28 12:28:45 -0400

    Add more parts to history list

--- src/main/java/ennuo/craftworld/things/PartHistory.java
+++ src/main/java/ennuo/craftworld/things/PartHistory.java
@@ -46,4 +46,24 @@ public class PartHistory {
     public static final int SWITCH_INPUT = 0x29;
     public static final int MICRO_CHIP = 0x2a;
     public static final int MATERIAL_TWEAK = 0x2b;
+    public static final int MATERIAL_OVERRIDE = 0x2c;
+    public static final int INSTRUMENT = 0x2d;
+    public static final int SEQUENCER = 0x2e;
+    public static final int CONTROLINATOR = 0x2f;
+    public static final int POPPET_POWERUP = 0x30;
+    public static final int POCKET_ITEM = 0x31;
+    public static final int CREATOR_ANIM = 0x32;
+    public static final int TRANSITION = 0x33;
+    public static final int FADER = 0x34;
+    public static final int ANIMATION_TWEAK = 0x35;
+    public static final int WIND_TWEAK = 0x36;
+    public static final int POWER_UP = 0x37;
+    public static final int HUD_ELEM = 0x38;
+    public static final int TAG_SYNCHRONIZER = 0x39;
+    public static final int WORMHOLE = 0x3a;
+    public static final int QUEST = 0x3b;
+    public static final int CONNECTOR_HOOK = 0x3c;
+    public static final int ATMOSHPERIC_TWEAK = 0x3d;
+    public static final int STREAMING_DATA = 0x3e;
+    public static final int STREAMING_HINT = 0x3f;
 }
