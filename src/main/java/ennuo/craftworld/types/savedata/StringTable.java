cb89711aaa4631eabee898b5cdfdfe17bf6d5943
ennuo <ajr.uiz2048@gmail.com>
2022-03-31 20:33:00 -0400

    Sort strings when writing StringTable

--- src/main/java/ennuo/craftworld/types/savedata/StringTable.java
+++ src/main/java/ennuo/craftworld/types/savedata/StringTable.java
@@ -3,6 +3,10 @@ package ennuo.craftworld.types.savedata;
 import ennuo.craftworld.resources.structs.StringEntry;
 import ennuo.craftworld.serializer.Serializable;
 import ennuo.craftworld.serializer.Serializer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class StringTable implements Serializable {
     public boolean unsorted;
@@ -12,6 +16,15 @@ public class StringTable implements Serializable {
     
     public StringTable serialize(Serializer serializer, Serializable structure) {
         StringTable table = (structure == null) ? new StringTable() : (StringTable) structure;
+        
+        if (serializer.isWriting && table.strings != null && table.strings.length != 0) {
+            ArrayList<StringEntry> strings = new ArrayList<>(Arrays.asList(table.strings));
+            strings.sort((e1, e2) -> e1.string.compareTo(e2.string));
+            table.rawIndexToSortedIndex = new int[table.strings.length];
+            for (int i = 0; i < table.strings.length; ++i)
+                table.rawIndexToSortedIndex[table.strings[i].index] = i;
+            table.strings = strings.toArray(StringEntry[]::new);
+        }
         
         table.unsorted = serializer.bool(table.unsorted);
         table.sortEnabled = serializer.bool(table.sortEnabled);
