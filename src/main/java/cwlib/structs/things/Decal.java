b14e7682185a557f3cda28814e630ddda216865a
ennuo <ajr.uiz2048@gmail.com>
2022-06-30 15:31:09 -0500

    Add PStickers + related structures

---
+++ src/main/java/cwlib/structs/things/Decal.java
@@ -0,0 +1,115 @@
+package cwlib.structs.things;
+
+import cwlib.enums.DecalType;
+import cwlib.enums.ResourceType;
+import cwlib.io.Serializable;
+import cwlib.io.serializer.Serializer;
+import cwlib.types.data.GUID;
+import cwlib.types.data.ResourceDescriptor;
+
+/**
+ * Represents a sticker on an object.
+ */
+public class Decal implements Serializable {
+    public static final int BASE_ALLOCATION_SIZE = 0x80;
+
+    /**
+     * The texture used for this decal.
+     */
+    private ResourceDescriptor texture;
+
+    /**
+     * Coordinates of decal on UV map.
+     */
+    private float u, v;
+
+    /**
+     * Rect of decal on UV map.
+     */
+    private float xvecu, xvecv, yvecu, yvecv;
+
+    /**
+     * Color tint of this decal (RGB565).
+     */
+    private short color;
+
+    /**
+     * Type of decal
+     */
+    private DecalType type = DecalType.STICKER;
+
+    /* No idea what this is actually used for, probably a reference to the thing containing the PMetaData for this sticker? */
+    private short metadataIndex, numMetadata;
+
+    /**
+     * Player that placed this decal.
+     */
+    private short placedBy;
+
+    /**
+     * Number of frames that have passed in play mode.
+     */
+    private int playModeFrame;
+
+    /**
+     * If this decal has a scorch mark on it.
+     */
+    private boolean scorchMark;
+
+    /**
+     * The plan that this decal came from.
+     */
+    private ResourceDescriptor plan;
+
+    @SuppressWarnings("unchecked")
+    @Override public Decal serialize(Serializer serializer, Serializable structure) {
+        Decal decal = (structure == null) ? new Decal() : (Decal) structure;
+
+        int version = serializer.getRevision().getVersion();
+
+        decal.texture = serializer.resource(decal.texture, ResourceType.TEXTURE);
+        decal.u = serializer.f32(decal.u);
+        decal.v = serializer.f32(decal.v);
+        decal.xvecu = serializer.f32(decal.xvecu);
+        decal.xvecv = serializer.f32(decal.xvecv);
+        decal.yvecu = serializer.f32(decal.yvecu);
+        decal.yvecv = serializer.f32(decal.yvecv);
+
+        if (version >= 0x260)
+            decal.color = serializer.i16(decal.color);
+        
+        if (version >= 0x158) {
+            decal.type = serializer.enum8(decal.type);
+            decal.metadataIndex = serializer.i16(decal.metadataIndex);
+            if (version <= 0x3ba)
+                decal.numMetadata = serializer.i16(decal.numMetadata);
+        }
+
+        if (version >= 0x214)
+            decal.placedBy = serializer.i16(decal.placedBy);
+        if (version >= 0x215)
+            decal.playModeFrame = serializer.i32(decal.playModeFrame);
+        
+        if (version >= 0x219)
+            decal.scorchMark = serializer.bool(decal.scorchMark);
+
+        if (version >= 0x34c)
+            decal.plan = serializer.resource(decal.plan, ResourceType.PLAN, true);
+        else if (version >= 0x25b) {
+            if (serializer.isWriting()) {
+                if (decal.plan == null || decal.plan.isHash()) 
+                    serializer.getOutput().i32(0);
+                else if (decal.plan.isGUID())
+                    serializer.getOutput().guid(decal.plan.getGUID());
+            } else {
+                GUID guid = serializer.getInput().guid();
+                if (guid == null) decal.plan = null;
+                else decal.plan = new ResourceDescriptor(guid, ResourceType.PLAN);
+            }
+        }
+
+        return decal;
+    }
+
+    @Override public int getAllocatedSize() { return Decal.BASE_ALLOCATION_SIZE; }
+}
--- src/main/java/cwlib/structs/things/parts/PStickers.java
+++ src/main/java/cwlib/structs/things/parts/PStickers.java
@@ -2,13 +2,70 @@ package cwlib.structs.things.parts;
 
 import cwlib.io.Serializable;
 import cwlib.io.serializer.Serializer;
+import cwlib.structs.things.Decal;
+import cwlib.structs.things.PaintControlPoint;
+import cwlib.structs.inventory.EyetoyData;
 
+/**
+ * Part that contains all the stickers
+ * placed on an object.
+ */
 public class PStickers implements Serializable {
+    public static final int BASE_ALLOCATION_SIZE = 0x60;
 
-    public PStickers serialize(Serializer serializer, Serializable structure) {
+    /**
+     * Decals that are on the primary thing.
+     */
+    public Decal[] decals;
+
+    /**
+     * Decals on specific costume pieces.
+     */
+    public Decal[][] costumeDecals;
+
+    /**
+     * Paint control data, depreciated.
+     */
+    public PaintControlPoint[] paintControl;
+
+    /**
+     * Eyetoy related information for decal placed.
+     */
+    public EyetoyData[] eyetoyData;
+
+    @SuppressWarnings("unchecked")
+    @Override public PStickers serialize(Serializer serializer, Serializable structure) {
         PStickers stickers = (structure == null) ? new PStickers() : (PStickers) structure;
+
+        int version = serializer.getRevision().getVersion();
+
+        stickers.decals = serializer.array(stickers.decals, Decal.class);
+
+        if (!serializer.isWriting()) stickers.costumeDecals = new Decal[serializer.getInput().i32()][];
+        else serializer.getOutput().i32(stickers.costumeDecals.length);
+        for (int i = 0; i < stickers.costumeDecals.length; ++i)
+            stickers.costumeDecals[i] = serializer.array(stickers.costumeDecals[i], Decal.class);
+
+        if (version >= 0x158 && version <= 0x3ba)
+            stickers.paintControl = serializer.array(stickers.paintControl, PaintControlPoint.class);
+        
+        if (version >= 0x15d)
+            stickers.eyetoyData = serializer.array(stickers.eyetoyData, EyetoyData.class);
         
         return stickers;
     }
-    
+
+    @Override public int getAllocatedSize() {
+        int size = BASE_ALLOCATION_SIZE;
+        if (this.decals != null)
+            size += (this.decals.length * Decal.BASE_ALLOCATION_SIZE);
+        for (Decal[] decals : this.costumeDecals)
+            if (decals != null) 
+                size += (decals.length * Decal.BASE_ALLOCATION_SIZE);
+        if (this.paintControl != null)
+            size += (this.paintControl.length * PaintControlPoint.BASE_ALLOCATION_SIZE);
+        if (this.eyetoyData != null)
+            size += (this.eyetoyData.length * EyetoyData.BASE_ALLOCATION_SIZE);
+        return size;
+    }
 }
