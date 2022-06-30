162d42da1d7fd6d5ee3b3db1ce6a9761237cd2c2
ennuo <ajr.uiz2048@gmail.com>
2022-06-30 11:26:49 -0500

    Fixup PGeneratedMesh

--- src/main/java/cwlib/structs/things/parts/PGeneratedMesh.java
+++ src/main/java/cwlib/structs/things/parts/PGeneratedMesh.java
@@ -1,34 +1,146 @@
 package cwlib.structs.things.parts;
 
-import cwlib.enums.ResourceType;
-import cwlib.io.Serializable;
-import cwlib.io.serializer.Serializer;
-import cwlib.types.data.ResourceDescriptor;
 import org.joml.Vector4f;
 
+import cwlib.enums.ResourceType;
+import cwlib.enums.VisibilityFlags;
+import cwlib.io.Serializable;
+import cwlib.io.serializer.Serializer;
+import cwlib.types.data.GUID;
+import cwlib.types.data.ResourceDescriptor;
+import cwlib.types.data.Revision;
+
+/**
+ * This part functionally serves as the rendering
+ * component for materials in games, this must
+ * be used alongside a PShape.
+ */
 public class PGeneratedMesh implements Serializable {
-    public ResourceDescriptor gfxMaterial;
+    public static final int BASE_ALLOCATION_SIZE = 0x80;
+
+    /**
+     * The material used to render this mesh.
+     */
+    public ResourceDescriptor gfxMaterial = 
+        new ResourceDescriptor(11166, ResourceType.GFX_MATERIAL);
+
+    /**
+     * The bevel used with this mesh.
+     */
     public ResourceDescriptor bevel;
-    public Vector4f uvOffset = new Vector4f(0, 0, 0, 0);
-    public long planGUID;
-    public boolean visible;
-    
-    public PGeneratedMesh serialize(Serializer serializer, Serializable structure) {
-        PGeneratedMesh generatedMesh = (structure == null) ? new PGeneratedMesh() : (PGeneratedMesh) structure;
-        
-        generatedMesh.gfxMaterial = serializer.resource(generatedMesh.gfxMaterial, ResourceType.GFX_MATERIAL);
-        generatedMesh.bevel = serializer.resource(generatedMesh.bevel, ResourceType.BEVEL);
-        
-        generatedMesh.uvOffset = serializer.v4(generatedMesh.uvOffset);
-        
-        if (serializer.revision.head > 0x257)
-            generatedMesh.planGUID = serializer.u32(generatedMesh.planGUID);
-        
-        if (serializer.revision.head < 0x27c) return generatedMesh;
-        
-        generatedMesh.visible = serializer.bool(generatedMesh.visible);
-        
-        return generatedMesh;
+
+    /**
+     * How much the UVs of the texture are offset.
+     * (UV tool in-game)
+     */
+    public Vector4f uvOffset;
+
+    /**
+     * The plan this generated mesh came from.
+     */
+    public GUID planGUID;
+
+    /**
+     * Flags controlling the visibility of this mesh.
+     */
+    public byte visibilityFlags = VisibilityFlags.PLAY_MODE | VisibilityFlags.EDIT_MODE;
+
+    /**
+     * Speed of this material's animation,
+     * animations are defined in the RGfxMaterial.
+     */
+    public float textureAnimationSpeed = 1.0f;
+
+    /**
+     * The speed of this material's animation when it's off,
+     * animations are defined in the RGfxMaterial.
+     */
+    public float textureAnimationSpeedOff = 1.0f;
+
+
+    /* Vita texture parameters */
+    public int uvMode;
+    public float textureScale;
+
+    /**
+     * Indicates that this material should have no bevel.
+     */
+    public boolean noBevel;
+
+    /**
+     * Whether or not this material has been sharded.
+     */
+    public boolean sharded;
+
+    /**
+     * Whether or not the sides should be rendered(?)
+     */
+    public boolean includeSides = true;
+
+
+    public byte slideImpactDamping;
+
+    /**
+     * Speed the player moves when steering on a slide.
+     */
+    public byte slideSteer;
+
+    /**
+     * Speed the player descends down this slide.
+     */
+    public byte slideSpeed;
+
+    @SuppressWarnings("unchecked")
+    @Override public PGeneratedMesh serialize(Serializer serializer, Serializable structure) {
+        PGeneratedMesh mesh = (structure == null) ? new PGeneratedMesh() : (PGeneratedMesh) structure;
+
+        Revision revision = serializer.getRevision();
+        int version = revision.getVersion();
+        int subVersion = revision.getSubVersion();
+
+        mesh.gfxMaterial = serializer.resource(mesh.gfxMaterial, ResourceType.GFX_MATERIAL);
+        mesh.bevel = serializer.resource(mesh.bevel, ResourceType.BEVEL);
+        mesh.uvOffset = serializer.v4(mesh.uvOffset);
+        if (version >= 0x258)
+            mesh.planGUID = serializer.guid(mesh.planGUID);
+
+        if (subVersion >= 0xb)
+            mesh.visibilityFlags = serializer.i8(mesh.visibilityFlags);
+        else {
+            if (serializer.isWriting())
+                serializer.getOutput().bool((mesh.visibilityFlags & VisibilityFlags.PLAY_MODE) != 0);
+            else {
+                mesh.visibilityFlags = VisibilityFlags.EDIT_MODE;
+                if (serializer.getInput().bool())
+                    mesh.visibilityFlags |=  VisibilityFlags.PLAY_MODE;
+            }
+        }
+
+        if (version >= 0x305) {
+            mesh.textureAnimationSpeed = serializer.f32(mesh.textureAnimationSpeed);
+            mesh.textureAnimationSpeedOff = serializer.f32(mesh.textureAnimationSpeedOff);
+        }
+
+        if (revision.isVita()) {
+            mesh.uvMode = serializer.i32(mesh.uvMode);
+            mesh.textureScale = serializer.f32(mesh.textureScale);
+        }
+
+        if (subVersion >= 0x34)
+            mesh.noBevel = serializer.bool(mesh.noBevel);
+        if (subVersion >= 0x97)
+            mesh.sharded = serializer.bool(mesh.sharded);
+        if (subVersion >= 0x13d)
+            mesh.includeSides = serializer.bool(mesh.includeSides);
+        if (subVersion >= 0x155)
+            mesh.slideImpactDamping = serializer.i8(mesh.slideImpactDamping);
+        if (subVersion >= 0x13d) {
+            mesh.slideSteer = serializer.i8(mesh.slideSteer);
+            mesh.slideSpeed = serializer.i8(mesh.slideSpeed);
+        }
+
+        return mesh;
     }
-    
+
+    @Override public int getAllocatedSize() { return BASE_ALLOCATION_SIZE; }
 }
