21bc7ebc3efb2c63c108a4188dfe26dff677f747
ennuo <ajr.uiz2048@gmail.com>
2022-09-04 13:34:25 -0400

    Executable switcher

--- src/main/java/toolkit/Launcher.java
+++ src/main/java/toolkit/Launcher.java
@@ -3,14 +3,38 @@ package toolkit;
 import com.github.weisj.darklaf.LafManager;
 import com.github.weisj.darklaf.theme.DarculaTheme;
 
+import executables.Jsoninator;
+import executables.gfx.GfxGUI;
 import toolkit.configurations.Config;
 import toolkit.utilities.ResourceSystem;
 import toolkit.windows.Toolkit;
 
 import java.awt.EventQueue;
+import java.util.Arrays;
 
 public class Launcher {
     public static void main(String args[]) {
+        // Executable switcher
+        if (args.length != 0) {
+            String mode = args[0];
+            String[] programArgs = Arrays.copyOfRange(args, 1, args.length);
+            
+            switch (mode) {
+                case "--jsoninator": case "-j": {
+                    Jsoninator.main(programArgs);
+                    return;
+                }
+                case "--gfx": case "-g": {
+                    GfxGUI.main(programArgs);
+                    return;
+                }
+            }
+
+            System.err.println("Invalid arguments!");
+            return;
+        }
+
+        // If no switcher is provided, use Toolkit GUI.
         LafManager.install(new DarculaTheme());
         Config.initialize();
         ResourceSystem.GUI_MODE = true;
--- src/main/java/toolkit/configurations/ApplicationFlags.java
+++ src/main/java/toolkit/configurations/ApplicationFlags.java
@@ -1,5 +1,9 @@
 package toolkit.configurations;
 
+import java.io.File;
+
+import cwlib.util.FileIO;
+
 /**
  * Flags controlling whether or not certain
  * features should be enabled or not, usually
@@ -15,5 +19,15 @@ public class ApplicationFlags {
      * Path to shader compiler executable, you'll have to
      * get this from a PS3 SDK yourself.
      */
-    public static String SCE_CGC_EXECUTABLE = "";
+    public static File SCE_CGC_EXECUTABLE = new File("E:/util/sce-cgc.exe");
+
+    /**
+     * Whether or not Toolkit can compile shaders, requires
+     * SCE_CGC_EXECUTABLE to be set.
+     */
+    public static boolean CAN_COMPILE_SHADERS = false;
+    static {
+        if (SCE_CGC_EXECUTABLE != null)
+            ApplicationFlags.CAN_COMPILE_SHADERS = SCE_CGC_EXECUTABLE.exists();
+    }
 }
