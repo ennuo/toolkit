fe359c54e18ea796f5e413cb19665e41b15189a7
ennuo <ajr.uiz2048@gmail.com>
2023-01-09 16:57:11 -0500

    Add flags and indices to CostumePieceCategory

--- src/main/java/cwlib/enums/CostumePieceCategory.java
+++ src/main/java/cwlib/enums/CostumePieceCategory.java
@@ -3,21 +3,32 @@ package cwlib.enums;
 import java.util.EnumSet;
 
 public enum CostumePieceCategory {
-    BEARD,
-    FEET,
-    EYES,
-    GLASSES,
-    MOUTH,
-    MOUSTACHE,
-    NOSE,
-    HAIR,
-    HEAD,
-    NECK,
-    TORSO,
-    LEGS,
-    HANDS,
-    WAIST;
-    
+    BEARD(0),
+    FEET(1),
+    EYES(2),
+    GLASSES(3),
+    MOUTH(4),
+    MOUSTACHE(5),
+    NOSE(6),
+    HAIR(7),
+    HEAD(8),
+    NECK(9),
+    TORSO(10),
+    LEGS(11),
+    HANDS(12),
+    WAIST(13);
+
+    private CostumePieceCategory(int index) {
+        this.index = index;
+        this.flag = (1 << index);
+    }
+
+    private final int index;
+    private final int flag;
+
+    public int getIndex() { return this.index; }
+    public int getFlag() { return this.flag; }
+
     public static int getFlags(EnumSet<CostumePieceCategory> set) {
         int flags = 0;
         CostumePieceCategory[] categories = CostumePieceCategory.values();
