4dfc384720df8af7225a676cb51d0df93eca55ec
Aidan <ajr.uiz2048@gmail.com>
2024-01-04 18:01:37 -0500

    Add toXML to RGfxMaterial

--- src/main/java/cwlib/structs/gmat/MaterialBox.java
+++ src/main/java/cwlib/structs/gmat/MaterialBox.java
@@ -1,11 +1,16 @@
 package cwlib.structs.gmat;
 
+import java.util.Arrays;
+
 import org.joml.Vector2f;
+import org.joml.Vector3f;
 import org.joml.Vector4f;
 
 import cwlib.enums.BoxType;
 import cwlib.io.Serializable;
 import cwlib.io.serializer.Serializer;
+import cwlib.resources.RGfxMaterial;
+import cwlib.util.XmlFormatter;
 
 public class MaterialBox implements Serializable {
     public static final int BASE_ALLOCATION_SIZE = 0x80;
@@ -112,4 +117,184 @@ public class MaterialBox implements Serializable {
 
     public boolean isColor() { return this.type == BoxType.COLOR; }
     public boolean isTexture() { return this.type == BoxType.TEXTURE_SAMPLE; }
+
+    public void toXML(RGfxMaterial material, XmlFormatter xml)
+    {
+        switch (type)
+        {
+            case BoxType.OUTPUT: return;
+            case BoxType.TEXTURE_SAMPLE: {
+
+                xml.startTag("Texture");
+
+                float rotation = 0.0f;
+                int channel = params[4];
+                int texunit = params[5];
+                Vector2f scale, offset;
+
+                if (subType == 0)
+                {
+                    scale = new Vector2f(
+                        Float.intBitsToFloat(params[0]),
+                        Float.intBitsToFloat(params[1])
+                    );
+
+                    offset = new Vector2f(
+                        Float.intBitsToFloat(params[2]),
+                        Float.intBitsToFloat(params[3])  
+                    );
+                }
+                else
+                {
+                    float[] rotation_matrix = new float[] 
+                    {
+                        Float.intBitsToFloat(params[0]),
+                        Float.intBitsToFloat(params[3]),
+                        Float.intBitsToFloat(params[1]),
+                        Float.intBitsToFloat(params[6]),
+                        Float.intBitsToFloat(params[2]),
+                        Float.intBitsToFloat(params[7])
+                    };
+
+                    Vector2f col0 = new Vector2f(rotation_matrix[0], rotation_matrix[1]);
+                    Vector2f col1 = new Vector2f(rotation_matrix[2], rotation_matrix[3]);
+
+                    scale = new Vector2f(col0.length(), col1.length());
+
+                    col0.div(scale.x);
+                    col1.div(scale.y);
+
+                    rotation = (float) Math.acos(col0.x);
+
+                    offset = new Vector2f(rotation_matrix[4], rotation_matrix[5]);
+
+                    xml.startTag("RotationMatrix");
+                        xml.addTag("Row", String.format("%f, %f, %f", rotation_matrix[0], rotation_matrix[2], rotation_matrix[4]));
+                        xml.addTag("Row", String.format("%f, %f, %f", rotation_matrix[1], rotation_matrix[3], rotation_matrix[5]));
+                    xml.endTag();
+                }
+
+                offset.x = (float) (offset.x + scale.y * Math.sin(rotation));
+                offset.y = (float) (1.0f - offset.y - scale.y * Math.cos(rotation));
+
+                xml.startTag("Scale");
+                    xml.addTag("x", scale.x);
+                    xml.addTag("y", scale.y);
+                xml.endTag();
+
+                xml.startTag("Offset");
+                    xml.addTag("x", offset.x);
+                    xml.addTag("y", offset.y);
+                xml.endTag();
+
+                xml.addTag("Rotation", String.format("%.1fdeg", Math.toDegrees(rotation)));
+
+                xml.addTag("Channel", channel);
+                xml.addTag("Texture", texunit);
+
+                xml.endTag();
+
+                break;
+            }
+            case BoxType.THING_COLOR:
+            {
+                xml.addTag("Color", "type=\"Thing\"", null);
+                break;
+            }
+            case BoxType.COLOR:
+            {
+                float[] rgba = new float[] 
+                {
+                    Float.intBitsToFloat(params[0]),
+                    Float.intBitsToFloat(params[1]),
+                    Float.intBitsToFloat(params[2]),
+                    Float.intBitsToFloat(params[3]),
+                };
+
+                String hex = String.format("#%02X%02X%02X%02X", 
+                    (int)(rgba[0] * 255.0f),
+                    (int)(rgba[1] * 255.0f),
+                    (int)(rgba[2] * 255.0f),
+                    (int)(rgba[3] * 255.0f)
+                );
+
+                xml.startTag("Color", "type=\"RGBA\"");
+                    xml.addTag("Float4", String.format("%f, %f, %f, %f", rgba[0], rgba[1], rgba[2], rgba[3]));
+                    xml.addTag("Hex", hex);
+                xml.endTag();
+
+                break;
+            }
+
+            case BoxType.CONSTANT:
+            {
+                xml.addTag("Float", params[0]);
+                break;
+            }
+
+            case BoxType.CONSTANT2:
+            {
+                xml.addTag("Float2", String.format("%f, %f", params[0], params[1]));
+                break;
+            }
+
+            case BoxType.CONSTANT3:
+            {
+                xml.addTag("Float3", String.format("%f, %f", params[0], params[1], params[2]));
+                break;
+            }
+
+            case BoxType.CONSTANT4:
+            {
+                xml.addTag("Float4", String.format("%f, %f, %f, %f", params[0], params[1], params[2], params[3]));
+                break;
+            }
+
+            case BoxType.MULTIPLY_ADD:
+            {
+                xml.startTag("MultiplyAdd");
+                    xml.startTag("Value");
+                        material.getBoxConnectedToPort(this, 0).toXML(material, xml);
+                    xml.endTag();
+                    xml.addTag("Multiplier", Float.intBitsToFloat(params[0]));
+                    xml.addTag("Addend", Float.intBitsToFloat(params[1]));
+                xml.endTag();
+
+                break;
+            }
+
+            case BoxType.MULTIPLY:
+            {
+                xml.startTag("Multiply");
+                    xml.startTag("Value");
+                        material.getBoxConnectedToPort(this, 0).toXML(material, xml);
+                    xml.endTag();
+                    xml.startTag("Value");
+                        material.getBoxConnectedToPort(this, 1).toXML(material, xml);
+                    xml.endTag();
+                xml.endTag();
+                break;
+            }
+
+            case BoxType.ADD:
+            {
+                xml.startTag("Add");
+                    xml.startTag("Value");
+                        material.getBoxConnectedToPort(this, 0).toXML(material, xml);
+                    xml.endTag();
+                    xml.startTag("Value");
+                        material.getBoxConnectedToPort(this, 1).toXML(material, xml);
+                    xml.endTag();
+                xml.endTag();
+                break;
+            }
+
+            default:
+            {
+                xml.startTag("Unsupported");
+                    xml.addTag("Message", "This node isn\'t supported, go yell at Aidan");
+                xml.endTag();
+            }
+        }
+    }
 }
